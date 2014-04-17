f0f2e6f092050f9ad251c27e7a1e320b399a6093
Federico Rota <federico.rota01@gmail.com>
2014-04-17 19:59:22 +0200

    Firs commit

---
+++ uidu/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Uidu::Application.config.secret_token = '020444896c4108cd9835657f08e38f6bc9ba6a0b58cb8fc3e25669a04bbb6dd89fe48d0c3664e52c5959d2164864e61a3c8569973ce96a382008416d41d29b04'
