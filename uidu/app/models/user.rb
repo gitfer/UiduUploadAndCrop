424fd5f83b654fe9b16e7cb58106fd41416ef4b2
Federico Rota <federico.rota01@gmail.com>
2014-04-18 10:43:53 +0200

    Validazioni server side paperclip

--- uidu/app/models/user.rb
+++ uidu/app/models/user.rb
@@ -1,21 +1,22 @@
 class User < ActiveRecord::Base
   attr_accessible :name, :avatar, :crop_x, :crop_y, :crop_w, :crop_h
   has_attached_file :avatar, :styles  => { :small => "100x100#", :large => "500x500>" }
-
-  attr_accessor :crop_x, :crop_y, :crop_w, :crop_h
+  validates_attachment_presence :photo
+  validates_attachment_size :photo, :less_than => 5.megabytes
+  validates_attachment_content_type :photo, :content_type => ['image/jpeg', 'image/pjpeg', 'image/png']
   after_update :reprocess_avatar, :if => :cropping?
-  
+
   def cropping?
     !crop_x.blank? && !crop_y.blank? && !crop_w.blank? && !crop_h.blank?
   end
-  
+
   def avatar_geometry(style = :original)
     @geometry ||= {}
     @geometry[style] ||= Paperclip::Geometry.from_file(avatar.path(style))
   end
-  
+
   private
-  
+
   def reprocess_avatar
     avatar.reprocess!
   end
