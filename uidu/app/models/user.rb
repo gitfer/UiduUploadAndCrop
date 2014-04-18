f99c0074ec4399fedf5ba3e9955e4a4c51a6cc61
Federico Rota <federico.rota01@gmail.com>
2014-04-18 11:15:35 +0200

    Fix field

--- uidu/app/models/user.rb
+++ uidu/app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ActiveRecord::Base
   attr_accessible :name, :avatar, :crop_x, :crop_y, :crop_w, :crop_h
   has_attached_file :avatar, :styles  => { :small => "100x100#", :large => "500x500>" }
-  validates_attachment_presence :photo
-  validates_attachment_size :photo, :less_than => 5.megabytes
-  validates_attachment_content_type :photo, :content_type => ['image/jpeg', 'image/pjpeg', 'image/png']
+  validates_attachment_presence :avatar
+  validates_attachment_size :avatar, :less_than => 5.megabytes
+  validates_attachment_content_type :avatar, :content_type => ['image/jpeg', 'image/pjpeg', 'image/png']
   after_update :reprocess_avatar, :if => :cropping?
 
   def cropping?
