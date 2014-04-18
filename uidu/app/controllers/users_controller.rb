59fefa68533fff1d9c736453f4c44c753ffd9a35
Federico Rota <federico.rota01@gmail.com>
2014-04-18 14:02:57 +0200

    Giro completo con jquery upload

--- uidu/app/controllers/users_controller.rb
+++ uidu/app/controllers/users_controller.rb
@@ -1,44 +1,49 @@
 class UsersController < ApplicationController
-def create
-  @user = User.new(params[:user])
-  if @user.save
-    flash[:notice] = "Successfully created user."
-    if params[:user][:avatar].blank?
-      redirect_to @user
-    else
-      render :action => 'crop'
-    end
-  else
-    render :action => 'new'
-  end
-end
 
-def update
-  @user = User.find(params[:id])
-  if @user.update_attributes(params[:user])
-    flash[:notice] = "Successfully updated user."
-    if params[:user][:avatar].blank?
-      redirect_to @user
-    else
-      render :action => 'crop'
-    end
-  else
-    render :action => 'edit'
-  end
-end
 
- def index
+  def create
+    @user = User.new(params[:user])
+    respond_to do |format|
+      if @user.save
+        flash[:notice] = "Successfully created user."
+        if params[:user][:avatar].blank?
+          redirect_to @user
+        else
+          # render :action => 'crop'
+          format.json { render json: {files: [@user.to_jq_upload]}, status: :created, location: @user }
+        end
+      else
+        render :action => 'new'
+      end
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:notice] = "Successfully updated user."
+      if params[:user][:avatar].blank?
+        redirect_to @user
+      else
+        render :action => 'crop'
+      end
+    else
+      render :action => 'edit'
+    end
+  end
+
+  def index
     @users = User.all
   end
-  
+
   def show
     @user = User.find(params[:id])
   end
-  
+
   def new
     @user = User.new
   end
-  
+
   def destroy
     @user = User.find(params[:id])
     @user.destroy
--- uidu/app/models/user.rb
+++ uidu/app/models/user.rb
@@ -15,9 +15,21 @@ class User < ActiveRecord::Base
     @geometry[style] ||= Paperclip::Geometry.from_file(avatar.path(style))
   end
 
+  include Rails.application.routes.url_helpers
+  def to_jq_upload
+    {
+      "name" => read_attribute(:avatar_file_name),
+      "size" => read_attribute(:avatar_file_size),
+      "url" => avatar.url(:original),
+      "delete_url" => user_path(self),
+      "delete_type" => "DELETE"
+    }
+  end
+
   private
 
   def reprocess_avatar
     avatar.reprocess!
   end
+
 end
